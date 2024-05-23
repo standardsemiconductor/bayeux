85bda84c2455fe18a7b1a5f61cff2a1b47c7d623
dopamane <dwc1295@gmail.com>
2024-05-22 23:11:07 -0700

    Render tableaux

--- src/Bayeux/Tableaux.hs
+++ src/Bayeux/Tableaux.hs
@@ -1,10 +1,25 @@
 {-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE LambdaCase        #-}
 
 module Bayeux.Tableaux
   ( Tableaux(..)
+  , renderTableaux
   ) where
 
 data Tableaux a = Leaf   a
                 | Stem   a (Tableaux a)
                 | Branch a (Tableaux a) (Tableaux a)
   deriving (Eq, Foldable, Functor, Read, Show, Traversable)
+
+renderTableaux :: Tableaux String -> String
+renderTableaux = unlines . draw
+  where
+    draw = \case
+      Leaf a       -> lines a
+      Stem a t     -> lines a ++ drawSubTrees [t]
+      Branch a l r -> lines a ++ drawSubTrees [l, r]
+    drawSubTrees = \case
+      []   -> []
+      [t]  -> "\x2502" : shift "\x2502" "" (draw t)
+      t:ts -> "\x2502" : shift "\x251C\x2500\x2510" "\x2502 " (draw t) ++ drawSubTrees ts
+    shift first other = zipWith (++) (first : repeat other)
