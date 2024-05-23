2d2bcae015a494c50f9b96d331fd4134425cece4
dopamane <dwc1295@gmail.com>
2024-05-23 02:17:02 -0700

    Order propositional rules
    Improve tableaux rendering

--- src/Bayeux.hs
+++ src/Bayeux.hs
@@ -4,9 +4,9 @@ import Bayeux.Cli
 import Bayeux.Lp
 import Data.Maybe
 import qualified Data.Text.IO as TIO
-import Text.Megaparsec
+import Text.Megaparsec hiding (parse)
 
 app :: Cli -> IO ()
-app cli = print . proveLp . fromJust =<< case input cli of
-  FileInput f -> parseMaybe (parseLp <* eof) <$> TIO.readFile f
-  StdInput    -> parseMaybe parseLp <$> TIO.getLine
+app cli = print . prove . fromJust =<< case input cli of
+  FileInput f -> parseMaybe (parse <* eof) <$> TIO.readFile f
+  StdInput    -> parseMaybe parse <$> TIO.getLine
--- src/Bayeux/Tableaux.hs
+++ src/Bayeux/Tableaux.hs
@@ -17,9 +17,11 @@ renderTableaux = unlines . draw
     draw = \case
       Leaf a       -> lines a
       Stem a t     -> lines a ++ drawSubTrees [t]
-      Branch a l r -> lines a ++ drawSubTrees [l, r]
+      Branch a l r -> lines a ++ drawSubTrees [r, l]
     drawSubTrees = \case
       []   -> []
       [t]  -> "\x2502" : shift "\x2502" "" (draw t)
-      t:ts -> "\x2502" : shift "\x251C\x2500\x2510" "\x2502 " (draw t) ++ drawSubTrees ts
+      t:ts -> case t of
+        Leaf{} -> "\x2502" : shift "\x251C\x2500 " "\x2502 " (draw t) ++ drawSubTrees ts
+        _      -> "\x2502" : shift "\x251C\x2500\x2510" "\x2502 " (draw t) ++ drawSubTrees ts
     shift first other = zipWith (++) (first : repeat other)
