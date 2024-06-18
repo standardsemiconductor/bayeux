7247122b914f45227939b9622e1ff83ce559e361
dopamane <dwc1295@gmail.com>
2024-06-17 23:39:58 -0700

    Implement Rgb demo using MonadSignal

--- lib/Bayeux/Rgb.hs
+++ lib/Bayeux/Rgb.hs
@@ -7,34 +7,42 @@ module Bayeux.Rgb
   , cycleProg
   ) where
 
-import Bayeux.Rtl
+import Bayeux.Rtl hiding (at, binary, process, mux)
+import Bayeux.Signal
+import Control.Monad
+import Control.Monad.Except
 import Control.Monad.Writer
+import Data.Word
 
 -- | PWM inputs, width=1
 class MonadRgb m where
-  rgb :: SigSpec -- ^ red
-      -> SigSpec -- ^ green
-      -> SigSpec -- ^ blue
+  rgb :: Sig -- ^ red
+      -> Sig -- ^ green
+      -> Sig -- ^ blue
       -> m ()
 
 instance MonadRgb Rtl where
-  rgb r g b = tell
-    [ ModuleBodyWire $ Wire [] $ WireStmt [WireOptionOutput 2] "\\red"
-    , ModuleBodyWire $ Wire [] $ WireStmt [WireOptionOutput 3] "\\green"
-    , ModuleBodyWire $ Wire [] $ WireStmt [WireOptionOutput 4] "\\blue"
-    , ModuleBodyCell $ sbRgbaDrv r g b
-    ]
+  rgb r g b = do
+    unless valid $ throwError SizeMismatch
+    tell
+      [ ModuleBodyWire $ Wire [] $ WireStmt [WireOptionOutput 2] "\\red"
+      , ModuleBodyWire $ Wire [] $ WireStmt [WireOptionOutput 3] "\\green"
+      , ModuleBodyWire $ Wire [] $ WireStmt [WireOptionOutput 4] "\\blue"
+      , ModuleBodyCell $ sbRgbaDrv (spec r) (spec g) (spec b)
+      ]
+    where
+      valid = size r == 1 && size g == 1 && size b == 1
 
-increment :: MonadRtl m => SigSpec -> m SigSpec
-increment a = binary addC False 32 False 32 32 a $ SigSpecConstant $ ConstantInteger 1
+increment :: Monad m => MonadSignal m => Sig -> m Sig
+increment a = binary addC a =<< (val . binaryValue) (1 :: Word32)
 
-ctr :: MonadRtl m => m SigSpec
-ctr = process 32 increment
+ctr :: Monad m => MonadSignal m => m Sig
+ctr = process False 32 increment
 
-eq :: MonadRtl m => SigSpec -> SigSpec -> m SigSpec
-eq = binary eqC False 32 False 32 1
+eq :: Monad m => MonadSignal m => Sig -> Sig -> m Sig
+eq a = flip at 0 <=< binary eqC a
 
-prog :: Monad m => MonadRtl m => MonadRgb m => m ()
+prog :: Monad m => MonadSignal m => MonadRgb m => m ()
 prog = do
   c <- ctr
   r <- c `at` 24
@@ -42,25 +50,23 @@ prog = do
   b <- c `at` 22
   rgb r g b
 
-cycleProg :: Monad m => MonadRtl m => MonadRgb m => m ()
+cycleProg :: Monad m => MonadSignal m => MonadRgb m => m ()
 cycleProg = do
-  t <- process 32 $ \timer -> do
+  zero   <- val $ binaryValue (0 :: Word32)
+  one    <- val $ binaryValue (1 :: Word32)
+  two    <- val $ binaryValue (2 :: Word32)
+  second <- val $ binaryValue (12000000 :: Word32)
+  t <- process False 32 $ \timer -> do
     t1Sec <- timer `eq` second
     timer' <- increment timer
-    mux 32 t1Sec timer' zero
+    mux t1Sec timer' zero
   tEqZ <- t `eq` zero
-  c <- process 32 $ \color -> do
+  c <- process False 32 $ \color -> do
     cEqBlue <- color `eq` two
     c' <- increment color
-    color' <- mux 32 cEqBlue c' zero
-    mux 32 tEqZ color color'
+    color' <- mux cEqBlue c' zero
+    mux tEqZ color color'
   pwmR <- c `eq` zero
   pwmG <- c `eq` one
   pwmB <- c `eq` two
   rgb pwmR pwmG pwmB
-  where
-    constSig = SigSpecConstant . ConstantInteger
-    zero   = constSig 0
-    one    = constSig 1
-    two    = constSig 2
-    second = constSig 12000000
