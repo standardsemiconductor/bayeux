f373801d8577ee412928ca7159da151fa53b74b1
dopamane <dwc1295@gmail.com>
2024-06-22 03:21:02 -0700

    Add cell module

---
+++ lib/Bayeux/Cell.hs
@@ -0,0 +1,41 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
+-- | Import this module qualified.
+module Bayeux.Cell
+  ( -- * Unary
+    inc
+  , logicNot
+  , not
+  , -- * Binary
+    add
+  , eq
+  , logicAnd
+  ) where
+
+import Bayeux.Rtl hiding (at, binary, unary)
+import Bayeux.Signal
+import Control.Monad
+import Data.Bits
+import Prelude hiding (not)
+
+-- | increment
+inc :: FiniteBits a => MonadSignal m => Sig a -> m (Sig a)
+inc a = binary addC a $ val True
+
+logicNot :: MonadSignal m => Sig Bool -> m (Sig Bool)
+logicNot = unary logicNotC
+
+not :: FiniteBits a => MonadSignal m => Sig a -> m (Sig a)
+not = unary notC
+
+add :: FiniteBits a => MonadSignal m => Sig a -> Sig a -> m (Sig a)
+add = binary addC
+
+eq :: FiniteBits a => Monad m => MonadSignal m => Sig a -> Sig a -> m (Sig Bool)
+eq a = flip at 0 <=< eq' a
+  where
+    eq' :: FiniteBits a => MonadSignal m => Sig a -> Sig a -> m (Sig a)
+    eq' = binary eqC
+
+logicAnd :: MonadSignal m => Sig Bool -> Sig Bool -> m (Sig Bool)
+logicAnd = binary logicAndC
