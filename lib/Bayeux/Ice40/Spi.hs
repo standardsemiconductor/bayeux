b4298757627341550598a4a8df4166c64cb4a6f7
dopamane <dwc1295@gmail.com>
2024-07-12 01:09:20 -0700

    Add IO cells

--- lib/Bayeux/Ice40/Spi.hs
+++ lib/Bayeux/Ice40/Spi.hs
@@ -5,7 +5,7 @@ module Bayeux.Ice40.Spi
   ( sbSpi
   , MonadSpi(..)
   , isW
-  , sliceAddress
+  , sliceAddr
   , sliceData
   ) where
 
@@ -167,8 +167,8 @@ instance Width Req where
 isW :: Sig Req -> Sig Bool
 isW = slice 12 12
 
-sliceAddress :: Sig Req -> Sig Word8
-sliceAddress = slice 11 8
+sliceAddr :: Sig Req -> Sig (Array (Finite 4) Bool)
+sliceAddr = slice 11 8
 
 sliceData :: Sig Req -> Sig Word8
 sliceData = slice 7 0
@@ -182,24 +182,149 @@ instance MonadSpi Rtl where
     let c   = SigSpecWireId "\\clk"
         rw  = isW req
         stb = isValid req
-        a7  = slice 7 7 $ sliceAddress req
-        a6  = slice 6 6 $ sliceAddress req
-        a5  = slice 5 5 $ sliceAddress req
-        a4  = slice 4 4 $ sliceAddress req
-        a3  = slice 3 3 $ sliceAddress req
-        a2  = slice 2 2 $ sliceAddress req
-        a1  = slice 1 1 $ sliceAddress req
-        a0  = slice 0 0 $ sliceAddress req
-        di7 = slice 7 7 $ sliceData    req
-        di6 = slice 6 6 $ sliceData    req
-        di5 = slice 5 5 $ sliceData    req
-        di4 = slice 4 4 $ sliceData    req
-        di3 = slice 3 3 $ sliceData    req
-        di2 = slice 2 2 $ sliceData    req
-        di1 = slice 1 1 $ sliceData    req
-        di0 = slice 0 0 $ sliceData    req
+        a7  = addr 7 busAddr -- slice 7 7 $ sliceAddr req
+        a6  = addr 6 busAddr -- slice 6 6 $ sliceAddr req
+        a5  = addr 5 busAddr -- slice 5 5 $ sliceAddr req
+        a4  = addr 4 busAddr -- slice 4 4 $ sliceAddr req
+        a3  = slice 3 3 $ sliceAddr req
+        a2  = slice 2 2 $ sliceAddr req
+        a1  = slice 1 1 $ sliceAddr req
+        a0  = slice 0 0 $ sliceAddr req
+        di7 = slice 7 7 $ sliceData req
+        di6 = slice 6 6 $ sliceData req
+        di5 = slice 5 5 $ sliceData req
+        di4 = slice 4 4 $ sliceData req
+        di3 = slice 3 3 $ sliceData req
+        di2 = slice 2 2 $ sliceData req
+        di1 = slice 1 1 $ sliceData req
+        di0 = slice 0 0 $ sliceData req
+    bi      <- freshWire 1
+    wi      <- freshWire 1
+    wcki    <- freshWire 1
+    wcsni   <- freshWire 1
+    do7     <- freshWire 1
+    do6     <- freshWire 1
+    do5     <- freshWire 1
+    do4     <- freshWire 1
+    do3     <- freshWire 1
+    do2     <- freshWire 1
+    do1     <- freshWire 1
+    do0     <- freshWire 1
+    acko    <- freshWire 1
+    irq     <- freshWire 1
+    wkup    <- freshWire 1
+    wo      <- freshWire 1
+    woe     <- freshWire 1
+    bo      <- freshWire 1
+    boe     <- freshWire 1
+    wcko    <- freshWire 1
+    wckoe   <- freshWire 1
+    bcsno3  <- freshWire 1
+    bcsno2  <- freshWire 1
+    bcsno1  <- freshWire 1
+    bcsno0  <- freshWire 1
+    bcsnoe3 <- freshWire 1
+    bcsnoe2 <- freshWire 1
+    bcsnoe1 <- freshWire 1
+    bcsnoe0 <- freshWire 1
     tell
       [ ModuleBodyCell $
           sbSpi name busAddr c rw stb a7 a6 a5 a4 a3 a2 a1 a0 di7 di6 di5 di4 di3 di2 di1 di0 bi wi wcki wcsni do7 do6 do5 do4 do3 do2 do1 do0 acko irq wkup wo woe bo boe wcko wckoe bcsno3 bcsno2 bcsno1 bcsno0 bcsnoe3 bcsnoe2 bcsnoe1 bcsnoe0
+      , ModuleBodyCell $ biwoIO biwo woe    wo    bi
+      , ModuleBodyCell $ bowiIO bowi boe    bo    wi
+      , ModuleBodyCell $ wckIO  wck  wckoe  wcko  wcki
+      , ModuleBodyCell $ csIO   cs   bcsnoe bcsno wcsni
       ]
     return $ Sig $ acko <> do7 <> do6 <> do5 <> do4 <> do3 <> do2 <> do1 <> do0
+
+biwo
+  :: SigSpec -- ^ package pin
+  -> SigSpec -- ^ output enable
+  -> SigSpec -- ^ D_OUT_0
+  -> SigSpec -- ^ D_IN_0
+  -> Cell
+biwo pp woe wo bi = sbIO
+  "\\SB_IO_BIWO"
+  (ConstantValue $ Value 6 [B0, B0, B0, B0, B0, B1]) -- pin type
+  (ConstantVlaue $ Value 1 [B0]) -- pullup
+  (ConstantValue $ Value 1 [B0]) -- neg trigger
+  (ConstantString "SB_LVCMOS")
+  pp
+  (SigSpecConstant $ ConstantValue $ Value 1 [B0])
+  (SigSpecConstant $ ConstantValue $ Value 1 [B0])
+  (SigSpecWireId "\\clk")
+  (SigSpecWireId "\\clk")
+  woe
+  wo
+  (SigSpecConstant $ ConstantValue $ Value 1 [B0])
+  bi
+  (SigSpecConstant $ ConstantValue $ Value 1 [B0])
+
+bowi
+  :: SigSpec -- ^ package pin
+  -> SigSpec -- ^ output enable
+  -> SigSpec -- ^ D_OUT_0
+  -> SigSpec -- ^ D_IN_0
+  -> Cell
+bowi pp boe bo wi = sbIO
+  "\\SB_IO_BOWI"
+  (ConstantValue $ Value 6 [B1, B0, B1, B0, B0, B1]) -- pin type
+  (ConstantValue $ Value 1 [B0]) -- pullup
+  (ConstantValue $ Value 1 [B0]) -- neg trigger
+  (ConstantString "SB_LVCMOS")
+  pp
+  (SigSpecConstant $ ConstantValue $ Value 1 [B0])
+  (SigSpecConstant $ ConstantValue $ Value 1 [B0])
+  (SigSpecWireId "\\clk")
+  (SigSpecWireId "\\clk")
+  boe
+  bo
+  (SigSpecConstant $ ConstantValue $ Value 1 [B0])
+  wi
+  (SigSpecConstant $ ConstantValue $ Value 1 [B0])
+
+wckIO
+  :: SigSpec
+  -> SigSpec
+  -> SigSpec
+  -> SigSpec
+  -> Cell
+wckIO pp wckoe wcko wcki = sbIO
+  "\\SB_IO_WCK"
+  (ConstantValue $ Value 6 [B1, B0, B1, B0, B0, B1]) -- pin type
+  (ConstantValue $ Value 1 [B1]) -- pullup
+  (ConstantValue $ Value 1 [B0]) -- neg trigger
+  (ConstantString "SB_LVCMOS")
+  pp
+  (SigSpecConstant $ ConstantValue $ Value 1 [B0])
+  (SigSpecConstant $ ConstantValue $ Value 1 [B0])
+  (SigSpecWireId "\\clk")
+  (SigSpecWireId "\\clk")
+  wckoe
+  wcko
+  (SigSpecConstant $ ConstantValue $ Value 1 [B0])
+  wcki
+  (SigSpecConstant $ ConstantValue $ Value 1 [B0])
+
+csIO
+  :: SigSpec
+  -> SigSpec
+  -> SigSpec
+  -> SigSpec
+  -> Cell
+csIO pp bcsnoe bcsno wcsni = sbIO
+  "\\SB_IO_CS"
+  (ConstantValue $ Value 6 [B1, B0, B1, B0, B0, B1]) -- pin type
+  (ConstantValue $ Value 1 [B1]) -- pullup
+  (ConstantValue $ Value 1 [B0]) -- neg trigger
+  (ConstantString "SB_LVCMOS")
+  pp
+  (SigSpecConstant $ ConstantValue $ Value 1 [B0])
+  (SigSpecConstant $ ConstantValue $ Value 1 [B0])
+  (SigSpecWireId "\\clk")
+  (SigSpecWireId "\\clk")
+  bcsnoe
+  bcsno
+  (SigSpecConstant $ ConstantValue $ Value 1 [B0])
+  wcsni
+  (SigSpecConstant $ ConstantValue $ Value 1 [B0])
