64207f4ea3d7f1e1a0703bdfecbf3f7697350184
David Cox <dwc1295@gmail.com>
2024-07-18 19:07:15 -0700

    MonadSpram (#36)

--- lib/Bayeux/Ice40/Spram.hs
+++ lib/Bayeux/Ice40/Spram.hs
@@ -1,10 +1,18 @@
+{-# LANGUAGE DataKinds         #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Bayeux.Ice40.Spram
   ( spramC
+  , MonadSpram(..)
   ) where
 
 import Bayeux.Rtl
+import Bayeux.Signal
+import Control.Monad.Writer
+import Data.Array
+import Data.Finite
+import Data.String
+import Data.Word
 
 spramC
   :: CellId
@@ -34,3 +42,32 @@ spramC name a din maskWrEn wren cs clk sb slp pwrOff dout = Cell
   , CellConnect "\\DATAOUT" dout
   ]
   CellEndStmt
+
+class MonadSpram m where
+  spram :: Sig (Array (Finite 14) Bool) -- ^ address
+        -> Sig Word16                   -- ^ data in
+        -> Sig (Array (Finite 4) Bool)  -- ^ mask write enable
+        -> Sig Bool                     -- ^ write enable
+        -> Sig Bool                     -- ^ chip select
+        -> Sig Bool                     -- ^ stand by
+        -> Sig Bool                     -- ^ sleep
+        -> Sig Bool                     -- ^ poweroff
+        -> m (Sig Word16)               -- ^ data out
+
+instance MonadSpram Rtl where
+  spram a din maskWrEn wren cs sb slp pwrOff = do
+    i    <- fresh
+    dout <- freshWire 16
+    tell [ModuleBodyCell $ spramC
+      (fromString $ "\\SB_SPRAM256KA_INST" <> show i)
+      (spec a)
+      (spec din)
+      (spec maskWrEn)
+      (spec wren)
+      (spec cs)
+      (SigSpecWireId "\\clk")
+      (spec sb)
+      (spec slp)
+      (spec pwrOff)
+      dout]
+    return $ Sig dout
