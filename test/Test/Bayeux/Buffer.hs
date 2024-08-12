3a087ca87cfbdde009ef9d2779dbc10044ce81cb
dopamane <dwc1295@gmail.com>
2024-08-12 15:25:44 +0200

    Add tests

--- test/Test/Bayeux/Buffer.hs
+++ test/Test/Bayeux/Buffer.hs
@@ -6,12 +6,15 @@ module Test.Bayeux.Buffer
 
 import Bayeux.Buffer
 import Bayeux.Width
+import Data.Word
 import Test.Tasty
 import Test.Tasty.HUnit
 
 tests :: [TestTree]
 tests =
   [ testGroup "width"
-    [ testCase "Cobuf" $ width (undefined :: Cobuf 1 Bool) @?= 4
+    [ testCase "Cobuf"     $ width (undefined :: Cobuf 1 Bool)            @?= 4
+    , testCase "CobufByte" $ width (undefined :: Cobuf 4 Word8)           @?= 36
+    , testCase "CobufBig"  $ width (undefined :: Cobuf 99 (Maybe Word32)) @?= 3276
     ]
   ]
